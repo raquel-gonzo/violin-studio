c9e23a98d7aefb03a96ddfe0e38e39e881731a43
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-10-07 20:32:53 -0500

    removed iframe elements.

--- client/src/components/StudentPortal.js
+++ client/src/components/StudentPortal.js
@@ -2,10 +2,30 @@ import React, { useEffect, useContext } from "react";
 import { useHistory } from "react-router-dom";
 import StudentContext from "../context/StudentContext";
 import TaskInput from "./TaskInput";
+import Embed from 'flat-embed';
 
 const StudentPortal = () => {
   const { studentData } = useContext(StudentContext);
   const history = useHistory();
+
+  const getScore = () => {
+    var container = document.getElementById('embed-container');
+    if (container != null) {
+      console.log(process.env);
+      console.log(process.env.REACT_APP_FLAT_EMBED_ID);
+      var embed = new Embed(container, {
+        width: "85%",
+        height: "900",
+        score: "5f7e53de891a521f30d475b2",
+        embedParams: {
+          appId: process.env.REACT_APP_FLAT_EMBED_ID,
+          controlsPosition: 'bottom',
+          layout: "responsive",
+        }
+      });
+    }
+  }
+
 
   useEffect(() => {
     const authToken = localStorage.getItem("auth-token");
@@ -30,8 +50,9 @@ const StudentPortal = () => {
         <TaskInput />
       </div>
 
-      <div>
-        <iframe
+      <div id='embed-container'>
+      {getScore()}
+        {/* <iframe
           title="g-minor"
           className="flatIO-element"
           src="https://flat.io/embed/5f5ac8f8c4feb36761f94c07?playbackMetronome=count-in"
@@ -39,7 +60,7 @@ const StudentPortal = () => {
           width="80%"
           frameBorder="0"
           allow="midi"
-        ></iframe>
+        ></iframe> */}
       </div>
     </div>
   );
