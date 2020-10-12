a7dd13e598454945003569f9c25f565cec1fb206
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-10-12 17:19:38 -0500

    add useCreateIndex to mongo connect config

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
 
-    mongoose.connect(
-        process.env.MONGODB_CONNECTION_STRING,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
+mongoose.connect(
+    process.env.MONGODB_CONNECTION_STRING,
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    })
+
+// const connection = mongoose.connection;
+// connection.once('open', () => { console.log('MongoDB Connected'); });
+// connection.on('error', (err) => { console.log('MongoDB connection error: ', err); }); 
 
     .then(() => console.log('Database is connected.'))
     .catch(err => console.log('Something went wrong when connecting to the DB ', err));
